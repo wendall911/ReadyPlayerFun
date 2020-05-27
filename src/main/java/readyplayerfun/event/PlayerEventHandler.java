6efbb943d9e2e1a7ee8c187104f0cd6337e6db6b
Wendall Cada <wendallc@83864.com>
2020-05-27 12:15:24 -0700

    Pausing ticks for both randomTick and fireTick using global game rules. Will prevent fire spread, crop growth, etc. when server is paused.

--- src/main/java/readyplayerfun/event/PlayerEventHandler.java
+++ src/main/java/readyplayerfun/event/PlayerEventHandler.java
@@ -7,6 +7,7 @@ import net.minecraft.server.management.PlayerList;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
 
+import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.FMLCommonHandler;
@@ -37,6 +38,8 @@ public class PlayerEventHandler {
     private int rainTime;
     private int thunderTime;
     private int weatherAutoSaveInterval;
+    private boolean doFireTick;
+    private int randomTickSpeed;
 
     @SubscribeEvent
     public void onPlayerLogin(final PlayerEvent.PlayerLoggedInEvent event) {
@@ -53,7 +56,7 @@ public class PlayerEventHandler {
                     player.sendMessage(new TextComponentString(msg));
                 }
 
-                unpauseServer(String.format("onPlayerLogin, %s", durationString));
+                unpauseServer(String.format("onPlayerLogin, %s", durationString), event.player.world);
             }
         }
     }
@@ -106,7 +109,7 @@ public class PlayerEventHandler {
             PlayerList playerList = FMLCommonHandler.instance().getMinecraftServerInstance().getPlayerList();
             checkTime = now;
             if (paused && playerList.getCurrentPlayerCount() >= 1) {
-                unpauseServer("onWorldTick");
+                unpauseServer("onWorldTick", world);
             }
             else if (!paused && playerList.getCurrentPlayerCount() <= 0) {
                 pauseServer(world, "init");
@@ -114,11 +117,23 @@ public class PlayerEventHandler {
         }
     }
 
+    @SubscribeEvent(priority = EventPriority.HIGHEST)
+    public void onWorldLoad(WorldEvent.Load event) {
+        World world = event.getWorld();
+        randomTickSpeed = world.getGameRules().getInt("randomTickSpeed");
+        doFireTick = world.getGameRules().getBoolean("doFireTick");
+    }
+
     private void pauseServer(World world, String ctx) {
         ReadyPlayerFun.logger.info(String.format("Pausing server %s", ctx));
         startPauseTime = System.currentTimeMillis();
         worldTime = world.getWorldTime();
         raining = world.getWorldInfo().isRaining();
+        randomTickSpeed = world.getGameRules().getInt("randomTickSpeed");
+        doFireTick = world.getGameRules().getBoolean("doFireTick");
+
+        world.getGameRules().setOrCreateGameRule("doFireTick", "false");
+        world.getGameRules().setOrCreateGameRule("randomTickSpeed", "0");
 
         if (Loader.isModLoaded("weather2")) {
             weatherAutoSaveInterval = ConfigMisc.Misc_AutoDataSaveIntervalInTicks;
@@ -137,17 +152,22 @@ public class PlayerEventHandler {
         else {
             weatherTime = world.getWorldInfo().getCleanWeatherTime();
         }
-        ReadyPlayerFun.logger.debug(String.format("Raining: %s rainTime: %d, thundering: %s, thunderTime %d, weatherTime: %d",
-                    raining, rainTime, thundering, thunderTime, weatherTime));
+
+        ReadyPlayerFun.logger.debug(String.format("(pauseServer) Raining: %s rainTime: %d, thundering: %s, thunderTime %d, weatherTime: %d, randomTickSpeed: %d, doFireTick: %s",
+                    raining, rainTime, thundering, thunderTime, weatherTime, randomTickSpeed, doFireTick));
 
         paused = true;
     }
 
-    private void unpauseServer(String ctx) {
+    private void unpauseServer(String ctx, World world) {
         if (Loader.isModLoaded("weather2")) {
             ConfigMisc.Misc_AutoDataSaveIntervalInTicks = weatherAutoSaveInterval;
             ServerTickHandler.initialize();
         }
+
+        world.getGameRules().setOrCreateGameRule("doFireTick", String.valueOf(doFireTick));
+        world.getGameRules().setOrCreateGameRule("randomTickSpeed", String.valueOf(randomTickSpeed));
+
         ReadyPlayerFun.logger.info(String.format("Unpausing server: %s", ctx));
 
         paused = false;
