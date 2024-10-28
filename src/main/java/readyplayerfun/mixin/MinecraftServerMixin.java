f57170af606379ad7768f6d0de284b0ec3166496
Wendall Cada <wendallc@83864.com>
2024-10-28 07:51:23 -0700

    Don't tick anything on server pause. Some mods are abusing the main thread and not just using world tick.

--- src/main/java/readyplayerfun/mixin/MinecraftServerMixin.java
+++ src/main/java/readyplayerfun/mixin/MinecraftServerMixin.java
@@ -48,7 +48,6 @@ public abstract class MinecraftServerMixin {
 
         // Tick essential things, don't tick world
         if (worldState.isPaused()) {
-            List<Runnable> tickables = ((MinecraftServerAccessor)server).getTickables();
             long curNanos = Util.getNanos();
 
             // Tick connection
@@ -56,9 +55,6 @@ public abstract class MinecraftServerMixin {
             server.getConnection().tick();
 
             profilerFiller.popPush("server gui");
-            for (int i = 0; i < tickables.size(); i++) {
-                tickables.get(i).run();
-            }
 
             // Handle console inputs normally
             ((DedicatedServer) server).handleConsoleInputs();
